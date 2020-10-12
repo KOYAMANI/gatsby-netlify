7c87eaff4f0ff1f8eb628a94bbf3bee6e5973762
小山雄太朗 <yutaro@koyamayuutaakiranoMacBook-Pro.local>
2020-10-12 20:22:11 +0200

    12 Oct 8:22

--- src/pages/about.js
+++ src/pages/about.js
@@ -29,8 +29,8 @@ const AboutPage = ({ location, title }) => (
         <li>1993年生まれ。</li>
         <li>2017年3月 5年間通った大学を卒業。</li>
         <li>2017年4月 家電メーカーに就職。関西で部品事業の国内外法人営業を担当。</li>
-        <li>2019年7月 シンガポール拠点に海外駐在員として出向。</li>
-        <li>2020年7月 退職。同9月より<a href="https://code.berlin/en/">CODE University of Applied Science</a>@ベルリン留学。エンジニア目指して絶賛修行中。</li>
+        <li>2019年7月 同社シンガポール拠点に駐在員として出向。</li>
+        <li>2020年7月 退職。同9月より<a href="https://code.berlin/en/">CODE University of Applied Science</a>@ベルリン留学。エンジニア目指して修行中。</li>
       <br></br>
       </ul>
     </div>
